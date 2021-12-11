5dacb24301c8f428dbe43c1a63f599fa0de62bf9
Samy Timalsina <samy.timalsina@auerprecision.com>
2021-12-10 20:00:27 -0700

    added InterfaceSegregationProperty

---
+++ OOPDesign/SOLID_PRINCIPLES/InterfaceSegration/Cuboid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDesign.SOLID_PRINCIPLES.InterfaceSegration
+{
+    public class Cuboid : IShape
+    {/// <summary>
+    /// no problem here, it has both area and volume
+    /// </summary>
+    /// <returns></returns>
+        public double calculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double calculateVolume()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OOPDesign/SOLID_PRINCIPLES/InterfaceSegration/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDesign.SOLID_PRINCIPLES.InterfaceSegration
+{
+    public class Square : IShape
+    {
+        //There is a problem on the OOP desing here
+        //it violates the SOLID principle
+        //Square does not require calculate volume method
+        public double calculateArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double calculateVolume()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
